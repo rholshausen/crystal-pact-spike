3269bc7d71098ad8d38b76f03bdc01cde0e8b907
Ronald Holshausen <ronald.holshausen@smartbear.com>
2024-09-10 10:05:16 +1000

    chore: Add basic todo provider

--- examples/simple/provider/src/provider.cr
+++ examples/simple/provider/src/provider.cr
@@ -1,18 +1,54 @@
-require "http/server"
+require "kemal"
 require "log"
-require "log/json"
+require "json"
 
 module Provider
   VERSION = "0.1.0"
+
+  class Task
+    include JSON::Serializable
+
+    @id : UInt32
+    @done : Bool = false
+    @name : String
+
+    def initialize(id : UInt32, name : String)
+      @id = id
+      @name = name
+    end
+  end
+
+  class Todo
+    include JSON::Serializable
+
+    @id : UInt32
+    @name : String
+    @type : String = "activity"
+    @due : Time?
+    @tasks : Array(Task) = [] of Task
+
+    def initialize(id : UInt32, name : String)
+      @id = id
+      @name = name
+    end
+  end
+
+  class Project
+    include JSON::Serializable
+
+    @todos = [] of Todo
+
+    def initialize
+    end
+  end
 end
 
 Log.setup_from_env
+project = Provider::Project.new
 
-server = HTTP::Server.new do |context|
-  context.response.content_type = "text/plain"
-  context.response.print "Hello world! The time is #{Time.local}"
+get "/" do |env|
+  env.response.content_type = "application/json"
+  project.to_json
 end
 
-address = server.bind_tcp 8080
-Log.info &.emit("Listening on http://#{address}", port: 8080)
-server.listen
+Kemal.run
