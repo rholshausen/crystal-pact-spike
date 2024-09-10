fe236da3ff7317629ab3aaf1e6b057da0b68b473
Ronald Holshausen <ronald.holshausen@smartbear.com>
2024-09-10 11:41:43 +1000

    chore: add shard

--- examples/simple/consumer/spec/consumer_spec.cr
+++ examples/simple/consumer/spec/consumer_spec.cr
@@ -6,8 +6,22 @@ describe Consumer do
     it "displays a list of todos from the provider" do
       project = Consumer::Project.new
 
-      WebMock.stub(:get, "http://localhost:3000/").
-        to_return(body: project.to_json, headers: {"Content-Type" => "application/json"})
+      WebMock.wrap do
+        WebMock.stub(:get, "http://localhost:3000/").
+          to_return(body: project.to_json, headers: {"Content-Type" => "application/json"})
+
+        Consumer.list
+      end
+    end
+  end
+
+  describe "list command pact test" do
+    before_each do
+      WebMock.allow_net_connect = true
+    end
+
+    it "displays a list of todos from the provider" do
+      project = Consumer::Project.new
 
       Consumer.list
     end
